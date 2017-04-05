528d02ceb0dcdd7b8ca8abad9f8a5217c374502f
Bartek <bartosz.z@pro-grom.pl>
2017-04-05 05:08:44 +0200

    init common files
    add:

--- src/System7/functions.php
+++ src/System7/functions.php
@@ -1,5 +1,29 @@
 <?php
+/*
+ * This file is part of the oXylion package.
+ *
+ * (c) Bartosz Zwierzchowski <vedget@pro-grom.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-function self_localize() {
-  echo dirname(__FILE__);
-}
+if (!function_exists('_print')) {
+    function _print($content)
+    {
+        if (is_array($content) or is_object($content)) {
+            print ("<pre>");
+            print_r($content);
+            print ("</pre>");
+        } else {
+            print ($content);
+        }
+    }
+} // end_of_if
+
+if (!function_exists('self_exists')) {
+    function self_localize()
+    {
+        echo dirname(__FILE__);
+    }
+} // end_of_if
